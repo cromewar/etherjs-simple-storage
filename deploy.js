0e569a162af80e3f897d34aa46a3a4f893fc9564
cromewar <vasiliy@cromewar>
2022-06-23 20:41:17 -0500

    deploying the contract using ganache

--- deploy.js
+++ deploy.js
@@ -1,4 +1,33 @@
-async function main() {}
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
+async function main() {
+  // Connnect to Ganache RPC
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://172.17.48.1:7545"
+  );
+
+  // Get Wallet trough private key
+  const wallet = new ethers.Wallet(
+    "260e9c3379c06653dbff401f764bfe25973ad83bf467752d424b132a06a726a5",
+    provider
+  );
+
+  // Take the ABI and the Binary
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf-8"
+  );
+
+  // Contract Factory for deployment
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("deploying please wait...");
+
+  // Deploy the contract
+  const contract = await contractFactory.deploy();
+  console.log(contract);
+}
 
 main()
   .then(() => process.exit(0))
